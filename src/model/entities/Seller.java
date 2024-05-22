00625bd3ce886afc5b8f34989d7d44262a160ba1
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-05-22 10:26:47 -0400

    Department entity class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,57 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private Date birthDate;
+	private Float baseSalary;
+	//private List<Departament> departament ArrayList<>();
+	
+	public Seller() {
+		
+	}
+	
+	public Seller(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+}
